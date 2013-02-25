81f18b7377d8f862b5ef73f4b7d1ad14023deb74
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2013-02-25 21:53:25 +0900

    Test added for InfoManager.py.

--- InfoManager.py
+++ InfoManager.py
@@ -1,20 +1,66 @@
-def main():
-    import os.path as op
-    info = InfoManager("test.xml")
-    diclist = [{"path":op.abspath("run01")}]
-    print info.metaPipe(diclist)
-
-
 class InfoManager(object):
     """
+    if specified optional, return elementTree object.
+
+    Usage:
+
+
+    >>> filestring = '''<?xml version="1.0" encoding="utf-8"?>
+    ... <data>
+    ...
+    ...   <run name="run01">
+    ...     <tags>
+    ...       <tag>yes</tag>
+    ...       <tag>no</tag>
+    ...       <tag>ahohage</tag>
+    ...     </tags>
+    ...     <comment>hogecommet</comment>
+    ...     <date>1987/05/12</date>
+    ...     <optional></optional>
+    ...   </run>
+    ...
+    ...   <run name="run02">
+    ...     <tags>
+    ...       <tag>tag1</tag>
+    ...       <tag>tag3</tag>
+    ...     </tags>
+    ...     <comment>come</comment>
+    ...     <date>1989/03/12</date>
+    ...   </run>
+    ...
+    ...   <run name="run03">
+    ...     <tags>
+    ...       <tag>tag321</tag>
+    ...       <tag>tag5</tag>
+    ...     </tags>
+    ...     <comment>hagehagehage</comment>
+    ...     <date>1988/02/03</date>
+    ...   </run>
+    ...
+    ... </data>
+    ... '''
+    >>> f = open("/tmp/test.xml", "w")
+    >>> f.write(filestring)
+    >>> f.close()
+    >>> import os.path as op
+    >>> info = InfoManager("/tmp/test.xml")
+    >>> diclist = [{"path":"/tmp/run01"}, {"path":"/tmp/run02"}]
+    >>> info.metaPipe(diclist)[1] == {'path': '/tmp/run02',
+    ...     'meta': {'comment': 'come', 'date': '1989/03/12',
+    ...         'optional': None,
+    ...         'tags': ['tag1', 'tag3']}}
+    True
+
+    # >>> import os
+    # >>> os.remove("/tmp/test.xml")
     """
 
     def __init__(self, info_path):
         import xml.etree.ElementTree as ET
         import os.path as op
-        tree = ET.parse(info_path)
-        self.root = tree.getroot()
         self.info_path = op.abspath(info_path)
+        tree = ET.parse(self.info_path)
+        self.root = tree.getroot()
         self.tags_nm = "tags"
         self.tag_nm = "tag"
         self.cmnt = "comment"
@@ -53,5 +99,4 @@ def _test():
     doctest.testmod()
 
 if __name__ == "__main__":
-    main()
     _test()
