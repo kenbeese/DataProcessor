1f24b146d5fcae6f4bfc06316cce24390aabc144
Toshiki Teramura <toshiki.teramura@gmail.com>
2013-02-26 01:39:58 +0900

    add processor.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+"""DataProcessor
+"""
+
+__all__ = ["filters","processor"]
+
+input_dic = {}
+output_dic = {}
---
+++ processor.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+from . import input_dic,output_dic
+from .filters import filter_dic
+
+def processing(action_list):
+    in_dic = input_dic[action_list["input"]["name"]]
+    run_list = in_dic["func"](in_dic["args"])
+
+    for f in action_list["filter"]:
+        f_dic = filter_dic[f["name"]]
+        run_list = f_dic["func"](run_list,f_dic["args"])
+
+    out_dic = output_dic[action_list["output"]["name"]]
+    out_dic["func"](run_list,out_dic["args"])
+
+def test():
+    action_list_sample = {
+            "input"  : {"name":"DirConstructor","args":[]},
+            "output" : {"name":"csv","args":[]},
+            "filter" : [
+                    {"name":"meta","args":[]},
+                    {"name":"project_choose","args":["P1"]}
+               ],
+        }
+    processing(action_list_sample)
+
