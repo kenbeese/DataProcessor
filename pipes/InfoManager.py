02a87b3f685088661885acc1cdb4763af0f6a497
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2013-03-03 06:16:14 +0900

    Add xml writer for human readable format.

--- pipes/InfoManager.py
+++ pipes/InfoManager.py
@@ -27,23 +27,15 @@ class InfoManager(object):
     ...     <comment>come</comment>
     ...     <date>1989/03/12</date>
     ...   </run>
-    ...
-    ...   <run name="run03">
-    ...     <tags>
-    ...       <tag> tag321   hoge</tag>
-    ...       <tag>tag5</tag>
-    ...     </tags>
-    ...     <comment>hagehagehage</comment>
-    ...     <date>1988/02/03</date>
-    ...   </run>
-    ...
-    ... </data>
+    ...   <run name="run03"><tags>  <tag> tag321   hoge</tag>   <tag>tag5</tag>  </tags> <comment>hagehagehage</comment>
+    ... <date>1988/02/03 </date></run></data>
     ... '''
     >>> f = open("/tmp/test.xml", "w")
     >>> f.write(filestring)
     >>> f.close()
     >>> import os.path as op
     >>> info = InfoManager("/tmp/test.xml")
+    >>> info.humanReadable("/tmp/human.xml")
     >>> diclist = [{"path":"/tmp/run01"}, {"path":"/tmp/run02"}, {"path":"/tmp/run03"}]
     >>> info.metaPipe(diclist)[1] == {'path': '/tmp/run02',
     ...     'meta': {'comment': 'come', 'date': '1989/03/12',
@@ -76,9 +68,13 @@ class InfoManager(object):
     >>> info.setComment('run03', "testdayo~")
     >>> info.setTag('run02', "testdayo~")
     >>> info.saveInfo('/tmp/hoge')
+    >>> info.addRun('run10')
+    >>> info.setTag('run10', "test2dayo")
+    >>> info.saveInfo()
     >>> import os
-    >>> os.remove("/tmp/test.xml")
+    >>> os.remove("/tmp/hoge")
     """
+
 
     def __init__(self, info_path):
         import xml.etree.ElementTree as ET
@@ -86,12 +82,14 @@ class InfoManager(object):
         self.info_path = op.abspath(info_path)
         self.tree = ET.parse(self.info_path)
         self.root = self.tree.getroot()
+        self.run_nm = "run"
         self.tags_nm = "tags"
         self.tag_nm = "tag"
         self.cmnt = "comment"
         self.opt = "optional"
         self.date = "date"
         self.metakey = "meta"
+
 
     def __path2elem(self, path):
         import os.path as op
@@ -100,6 +98,7 @@ class InfoManager(object):
             if elem.get("name") == path_key:
                 return elem
         return None
+
 
     def __elem2dict(self, elem):
         tags = [self.normWhiteSpace(tag.text)
@@ -135,8 +134,6 @@ class InfoManager(object):
              for tag in run.find(self.tags_nm).findall(self.tag_nm)
              if self.normWhiteSpace(tag.text) == self.normWhiteSpace(tagbody)]
         return l
-
-
 
 
     def runnamelist(self):
@@ -200,13 +197,122 @@ class InfoManager(object):
 
 
     def saveInfo(self, info_path = None):
+        """
+        if info_path is not specified, meta data is written in read file.
+        """
         import os.path
         if info_path == None:
             info_path = self.info_path
         else:
             info_path = os.path.abspath(info_path)
-
         self.tree.write(info_path)
+        return
+
+
+
+
+    def humanReadable(self, out_path = None):
+        import os.path
+
+        if out_path == None:
+            out_path = self.info_path
+        else:
+            out_path = os.path.abspath(out_path)
+
+        f = open(self.info_path)
+        string = f.read()
+        f.close()
+
+        string = _rmindent(string)
+        string = _splitTag(string)
+        string = _addNewline(string)
+        string = _indent(string)
+        f = open(out_path, "w")
+        f.write(string)
+        f.close
+
+        return
+
+    def addRun(self, runname):
+        import xml.etree.ElementTree as ET
+
+        for run in list(self.root):
+            if run.get("name") == runname:
+                raise Warning("%s exists already." % runname)
+        run = ET.SubElement(self.root, self.run_nm, {"name":runname})
+        ET.SubElement(run, self.cmnt)
+        ET.SubElement(run, self.tags_nm)
+        ET.SubElement(run, self.opt)
+        ET.SubElement(run, self.date)
+
+
+
+
+
+def _rmindent(string):
+    import re
+    sp = re.compile("[\n\r]")
+    lines = sp.split(string)
+    string = ""
+    for line in lines:
+        if line != "":
+            string = string + line.strip() + "\n"
+    return string
+
+def _splitTag(string):
+    import re
+    reg1 = re.compile(r"<(.*?)>[ \t\b]*<(.*?)>")
+    if reg1.search(string) == None:
+        return string
+    else:
+        string = reg1.sub(r"<\1>\n<\2>", string, 1)
+        return _splitTag(string)
+
+def _addNewline(string):
+    import re
+    reg1 = re.compile(r"(</run>[ \t\b]*[\n\r])(\S+)")
+    if reg1.search(string) == None:
+        return string
+    else:
+        string = reg1.sub(r"\1\n\2", string, 1)
+        return _addNewline(string)
+
+def _checkTag(line):
+    import re
+    startend = re.compile("<.+?>.*?</.+?>")
+    end = re.compile("</.+?>")
+    onetag = re.compile("<.+?/>")
+    start = re.compile("<.+?>")
+    if startend.search(line):
+        return "startend"
+    elif onetag.search(line):
+        return "oneline"
+    elif end.search(line):
+        return "end"
+    elif start.search(line):
+        return "start"
+    else:
+        return "none"
+
+def _indent(string):
+    import re
+    sp = re.compile("[\n\r]")
+    lines = sp.split(string)
+    string = ""
+    depth = 0
+    for line in lines:
+        if line == "":
+            string = string + "\n"
+        else:
+            if _checkTag(line) == "start":
+                string = string + "  " * (depth) + line + "\n"
+                depth = depth + 1
+            elif _checkTag(line) == "end":
+                depth = depth - 1
+                string = string + "  " * (depth) + line + "\n"
+            else:
+                string = string + "  " * (depth) + line + "\n"
+    return string
 
 
 def addRunsMeta(run_list, info_path):
@@ -219,7 +325,7 @@ def register(pipes_dics):
     pipes_dics["run_meta"] = {
         "func" : addRunsMeta,
         "args" : ["info_path"],
-        "desc" : "add run meta-data"
+        "desc" : "add run meta-data",
         }
 
 
