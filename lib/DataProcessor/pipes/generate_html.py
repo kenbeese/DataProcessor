83869e0060a0db66230a838201efd92c719a24ab
Toshiki Teramura <toshiki.teramura@gmail.com>
2013-04-20 18:50:42 +0900

    enable to design pre_meta/post_meta and confs

--- lib/DataProcessor/pipes/generate_html.py
+++ lib/DataProcessor/pipes/generate_html.py
@@ -1,23 +1,32 @@
 # coding=utf-8
 
-from jinja2 import Template
-def replace(run_list,template_filename,output_filename):
-    result_list = []
+def _get_confs(run_list):
     confs = []
     for run in run_list:
-        if "path" not in run or "meta" not in run or "configure" not in run:
-            continue
         conf = run["configure"]
         for c in conf:
             if c not in confs:
                 confs.append(c)
-        result_list.append(run)
     confs.sort()
     confs.sort(key=len)
-    cfg = { "run_list" : result_list,
+    return confs
+
+def _strip_invalid_run(run_list):
+    result_list = []
+    for run in run_list:
+        if "path" in run and "meta" in run and "configure" in run:
+            result_list.append(run)
+    return result_list
+
+from jinja2 import Template
+def replace(run_list,template_filename,output_filename,pre_meta,post_meta,confs):
+    stripped_list = _strip_invalid_run(run_list)
+    if not confs:
+        confs = _get_confs(stripped_list)
+    cfg = { "run_list" : stripped_list,
             "confs" : confs,
-            "pre_meta" : ["name","date"],
-            "post_meta" : ["tags","comment"]
+            "pre_meta" : pre_meta,
+            "post_meta" : post_meta,
         }
     with open(template_filename,'r') as f:
         tmpl = Template(f.read())
@@ -48,18 +57,18 @@ def _install_attachments(template_dir,dest_path):
         for a in glob(os.path.join(path,u"*."+att)):
             shutil.copy2(a,att_dest_dir)
 
-def generate(run_list,template_dir,output_html_path,template_html_filename="template.html",install_attachments=False):
+def generate(run_list,template_dir,output_html_path,template_html_filename="template.html",install_attachments=False,pre_meta=["name","date"],post_meta=["tags","comment"],confs=None):
     output_html_path = os.path.expanduser(output_html_path)
     dest_dir = os.path.dirname(output_html_path)
     if install_attachments:
         _install_attachments(template_dir,dest_dir)
     template_html_path = os.path.join(template_dir,template_html_filename)
-    replace(run_list,template_html_path,output_html_path)
+    replace(run_list,template_html_path,output_html_path,pre_meta,post_meta,confs)
 
 def register(pipes_dics):
     pipes_dics["generate_html"] = {
         "func" : generate,
         "args" : ["template_dir","output_html_path"],
         "desc" : "generate HTML and install attachments",
-        "kwds" : ["template_html_filename","install_attachments"],
+        "kwds" : ["template_html_filename","install_attachments","pre_meta","post_meta","confs"],
         }
