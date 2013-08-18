87a33e86e80ee26402c09b305331a61c37bb87f6
Toshiki Teramura <toshiki.teramura@gmail.com>
2013-08-18 12:23:41 +0900

    Add register function

--- lib/dataprocessor/pipes/figure.py
+++ lib/dataprocessor/pipes/figure.py
@@ -2,10 +2,7 @@
 import os.path
 
 
-def add_figure_node(node_list, path, figure_names,
-                    parents, children):
-    # check
-    path = os.path.expanduser(os.path.abspath(path))
+def _check_files(path, figure_names):
     if not os.path.exists(path):
         raise RuntimeError("figure directory does not found")
     for fig_name in figure_names:
@@ -13,7 +10,30 @@ def add_figure_node(node_list, path, figure_names,
         if not os.path.exists(fig_path):
             raise RuntimeError("figure %s does not found" % fig_name)
 
-    # create node
+
+def add_figure_node(node_list, path, figure_names,
+                    parents, children, check_files=True):
+    """
+    add figure node into node_list
+
+    >>> node_list = []
+    >>> node_list = add_figure_node(
+    ...                 node_list,"/tmp/doctest/add_figure_node/fig1",
+    ...                 ["fig1.eps","fig1.png"],
+    ...                 ["/tmp/doctest/add_figure_node"],
+    ...                 ["/path/to/run/data"],
+    ...                 check_files=False
+    ...             )
+    >>> node_list == [{'path': '/tmp/doctest/add_figure_node/fig1',
+    ...                'type': 'figure',
+    ...                'figures': ['fig1.eps', 'fig1.png'],
+    ...                'parents': ['/tmp/doctest/add_figure_node'],
+    ...                'children': ['/path/to/run/data']}]
+    True
+    """
+    path = os.path.expanduser(os.path.abspath(path))
+    if check_files:
+        _check_files(path, figure_names)
     node = {
         "path": path,
         "type": "figure",
@@ -22,3 +42,20 @@ def add_figure_node(node_list, path, figure_names,
         "children": children,
     }
     node_list.append(node)
+    return node_list
+
+
+def register(pipe_dics):
+    pipe_dics["add_figure_node"] = {
+        "func": add_figure_node,
+        "args": ["path", "figure_names", "parents", "children"],
+        "desc": "add figure node into node_list",
+    }
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == "__main__":
+    _test()
