565f5e59803652986cbdbaa16ded0f099c873143
Toshiki Teramura <toshiki.teramura@gmail.com>
2013-09-18 20:58:26 +0900

    Add doctest for node.get

--- lib/dataprocessor/nodes.py
+++ lib/dataprocessor/nodes.py
@@ -2,6 +2,26 @@
 
 
 def get(node_list, path):
+    """
+    search node from node_list by its path
+
+    >>> node_list = [{"path": "/path/1"}, {"path": "/path/2"},
+    ...              {"path": "/path/3"}, {"path": "/path/4"}]
+    >>> get(node_list, "/path/3")
+    {'path': '/path/3'}
+    >>> node = get(node_list, "/path/5") # no node exists
+    >>> print(node)
+    None
+    """
     for node in node_list:
         if path == node["path"]:
             return node
+    return None
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == "__main__":
+    _test()
