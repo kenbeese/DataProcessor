aef6aa0531296d74674831265d92f15f5b63380a
Toshiki Teramura <toshiki.teramura@gmail.com>
2013-09-22 00:54:50 +0900

    Rename functions

--- lib/dataprocessor/figure.py
+++ lib/dataprocessor/figure.py
@@ -12,12 +12,21 @@ class DataProcessorFigureError(Exception):
         return repr(self.msg)
 
 
-def create_node(figure_path, incidentals_path, parents, base_dir,
-                without_copy=False):
-    if without_copy:
+def create_node(figure_path, incidentals_path, parents, base_dir):
+    """
+    copy figure and incidentals and return corresponding node
+    """
+    path = copy(figure_path, incidentals_path, base_dir)
+    return node(figure_path, parents, base_dir, path)
+
+
+def node(figure_path, parents, base_dir, path=None):
+    """
+    Return node (does not copy)
+    """
+    if not path:
         path = _generate_path(figure_path, base_dir)
-    else:
-        path = _copy(figure_path, incidentals_path, base_dir)
+    _check_path(path)
     return {
         "path": path,
         "type": "figure",
@@ -27,11 +36,10 @@ def create_node(figure_path, incidentals_path, parents, base_dir,
     }
 
 
-def calc_hash(figure_path):
-    return hashlib.sha1(open(figure_path, 'r').read()).hexdigest()
-
-
-def _copy(fig_path, incidentals_path, base_dir):
+def copy(fig_path, incidentals_path, base_dir):
+    """
+    copy figure and incidentals
+    """
     _check_path(fig_path)
     dest_path = _generate_path(fig_path, base_dir)
     os.mkdir(dest_path)
@@ -39,6 +47,14 @@ def _copy(fig_path, incidentals_path, base_dir):
     for inc_path in incidentals_path:
         shutil.copy2(inc_path, dest_path)
     return dest_path
+
+
+zero_hash = hashlib.sha1("")
+def calc_hash(figure_path):
+    hash_str = hashlib.sha1(open(figure_path, 'r').read()).hexdigest()
+    if hash_str == zero_hash:
+        raise DataProcessorFigureError("Figure is empty.")
+    return hash_str
 
 
 def _generate_path(figure_path, base_dir):
@@ -57,3 +73,8 @@ def _check_path(path):
 
 def _path_expand(path):
     return os.path.expanduser(os.path.abspath(path))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
