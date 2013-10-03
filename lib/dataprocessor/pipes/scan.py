4ecf0fe2bab1470b3bb79ee0e3ae4ef3e1096e70
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2013-10-03 14:14:46 +0900

    Remove debug information.

--- lib/dataprocessor/pipes/scan.py
+++ lib/dataprocessor/pipes/scan.py
@@ -160,7 +160,6 @@ def directory(node_list, root, whitelist):
     origin_len = len(node_list)
     node_list = node_list + scan_nodelist
     for node in node_list[origin_len:]:
-        print node
         validate_link(node_list, node, ask_remove=False)
     return node_list
 
