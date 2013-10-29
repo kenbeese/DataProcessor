6ede674c3df00b2fa380b1729478fb33e20e6b99
Toshiki Teramura <toshiki.teramura@gmail.com>
2013-10-29 20:48:22 +0900

    Refine utilities

---
+++ lib/dataprocessor/io.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+from . import utility
+
+import json
+import os.path
+
+
+def save(node_list, json_path, ask_replace=True):
+    """
+    save node_list into a JSON file
+    """
+    path = utility.path_expand(json_path)
+    if ask_replace and os.path.exists(path):
+        ans = raw_input("File %s already exists. Replace? [y/N]"
+                        % json_path).lower()
+        if ans not in ["yes", "y"]:
+            print("Skip save_json pipe.")
+            return node_list
+    with open(path, "w") as f:
+        json.dump(node_list, f, indent=4)
+    return node_list
+
+
+def load(node_list, json_path):
+    """
+    load node_list from a JSON file
+    """
+    path = os.path.expanduser(json_path)
+    if not os.path.exists(path):
+        raise RuntimeError("json path does not exist")
+    with open(path, "r") as f:
+        read_node_list = json.load(f)
+    return node_list + read_node_list
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
