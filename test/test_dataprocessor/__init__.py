52b2294e0bb0aba93313e77bd0d772fc2b7d9891
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2013-11-28 04:02:18 +0900

    Modified import declaration.

--- test/test_dataprocessor/__init__.py
+++ test/test_dataprocessor/__init__.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import glob
 import os.path
+from . import test_pipes
 
 __all__ = [os.path.basename(f)[:-3]
            for f in glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
