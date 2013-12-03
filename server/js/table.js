72544ed468cd1ea51fcfbeea634a9bc2292b10da
Toshiki Teramura <toshiki.teramura@gmail.com>
2013-12-03 23:43:06 +0900

    Fully refactored #77

--- server/js/table.js
+++ server/js/table.js
@@ -1,44 +1,45 @@
-/**
- * send comment to server
- */
-function send_comment(comment, path){
-    $.post("/cgi-bin/api.cgi",
-        {
-            "type": "pipe",
-            "name": "add_comment",
-            "args": JSON.stringify([comment, path])
-        }, notice_api_result, "json"
-    );
-}
-
-
-/**
- * generate a wedget <div/> in section#Widgets
- */
-function widget_html(title, widgets){
-    var $widget_html = $("<div>").addClass("Widget");
-    $("<h2>")
-        .addClass("WidgetTitle")
-        .text(title)
-        .appendTo($widget_html);
-    $("<button>")
-        .text("hide")
-        .bind("click", function(event){
-            $(this).parent().remove();
-        })
-        .appendTo($widget_html);
-
-    for(var i=0; i<widgets.length; i++){
-        $(widgets[i])
-            .addClass("WidgetBody")
-            .appendTo($widget_html);
+function correct_width_table($table_object){
+    /** correct table width
+     *
+     *  +-------+----+    +-------+----+    +-------+
+     *  |  G1   | G2 |    |  G1   | G2 |    |G1 | G2|
+     *  +---+---+----+ >> +---+---+----+ >> +---+---+
+     *  | a | b | c  |    | a | c |    |    | a | c |
+     *  +---+---+----+    +---+---+----+    +---+---+
+     *                 ^^                ^^
+     *               by others       this function
+     */
+    var $groups = $table_object.find("thead>tr.group>th");
+    // create group name list
+    var group_list = [];
+    $groups.each(function(){
+        group_list.push($(this).data("group"));
+    });
+    // count group number
+    var group_num = {};
+    for (var i in group_list){
+        group_num[group_list[i]] = 0;
     }
-    create_editable_table($widget_html);
-    return $widget_html;
+    // count each group number
+    $table_object.parent().find("div.hide-show div>span.item").each(function(){
+        if ($(this).hasClass("show")) {
+            for (var j in group_list){
+                if ($(this).data("group") == group_list[j]){
+                    group_num[group_list[j]] += 1;
+                    break;
+                }}}
+    });
+    // correct width of table.
+    $groups.each(function(){
+        $(this).attr("colspan", group_num[$(this).data("group")]);
+        if (group_num[$(this).data("group")] == 0){
+            $(this).hide();
+        } else {
+            $(this).show();
+        }});
 }
 
-
-function create_editable_table($widget_html){
+function add_menu($widget_html){
 
     // create hide and show checkbox
     var $items = $widget_html.find("table.childrenTableWidget>thead>tr.items>th");
@@ -94,29 +95,34 @@ function create_editable_table($widget_html){
     correct_width_table($widget_html.find("table.childrenTableWidget"));
 }
 
-/**
- * ready table
- */
-function ready_table(){
-    $("section, nav#Projects")
+function enable_name_link($wrap_table){
+    /** enable link on name table data
+     *
+     * Parameters
+     * ----------
+     * wrap_table : wrap set including tables
+     */
+    $wrap_table
         .off("click", "table>tbody>tr>td.name")
-        .off("click", "table>tbody>tr>td.comment")
-        .off("blur", "table>tbody>tr>input.comment");
-    $("section, nav#Projects")
         .on("click", "table>tbody>tr>td.name", function(event){
             var name = this.innerHTML;
             var path = $(this).parent().attr("path");
-            $.getJSON("/cgi-bin/body.cgi",
-                {
-                    "type": "Widgets",
-                    "path": path,
-                    "table_type": "children"
-                }, function(widgets){
-                    var $widget_html = widget_html(name, widgets);
-                    $("section#Widgets").append($widget_html);
-                    ready_table();
-                });
-        })
+            get_widgets(path, function($widget){
+                add_widget(name, $widget);
+                ready_table();
+            })
+        });
+}
+
+function enable_editable_comment($wrap_table){
+    /** enable editable comment
+     *
+     * Parameters
+     * ----------
+     * wrap_table : wrap set including tables
+     */
+    $wrap_table
+        .off("click", "table>tbody>tr>td.comment")
         .on("click", "table>tbody>tr>td.comment", function(event){
             var comment = this.innerHTML;
             var path = $(this).parent().attr("path");
@@ -129,6 +135,7 @@ function ready_table(){
                 );
             $parent.find("input.comment").focus();
         })
+        .off("blur", "table>tbody>tr>input.comment")
         .on("blur", "table>tbody>tr>input.comment", function(event){
             var comment = this.value;
             var path = $(this).parent().attr("path");
@@ -140,144 +147,103 @@ function ready_table(){
                     .attr("path", path)
                 );
         });
+}
 
-/**
- * Sort table
- */
+function enable_sort($wrap_table){
+    /** enable sort
+     *
+     * Parameters
+     * ----------
+     * wrap_table : wrap set including tables
+     */
     var sortOrder = 1;
-    $("section").off("click", "div.Widget>table>thead>tr.items>th");
-    $("section").on("click", "div.Widget>table>thead>tr.items>th",
-                    function(){
-                        // get tbody
-                        var $rows = $(this).parents("table").children("tbody").find("tr");
-                        var col = $(this).index();
-
-                        // Sort
-                        $rows.sort(function(a, b){
-                            return compare(a, b, col) * sortOrder;
-                        });
-                        $(this).parents("table").children("tbody")
-                            .empty().append($rows);
-
-                        // update mark of sort order
-                        var arrow = sortOrder === 1 ? "▲" : "▼";
-                        $(this).parent().find('span').remove();
-                        var span = $('<span>');
-                        $(this).prepend(span);
-                        $(this).find('span').text(arrow);
-
-                        // change sort order
-                        sortOrder *= - 1;
- });
-    function compare(a, b, col) {
-        var _a = $(a).find('td').eq(col).text();
-        var _b = $(b).find('td').eq(col).text();
-        if (isNaN(parseFloat(_a))) {
-            // var __a = _a.toLowerCase();
-            // var __b = _b.toLowerCase();
-            var __a = _a;
-            var __b = _b;
-            if (__a < __b) //sort string ascending
-                return -1;
-            if (__a > __b)
-                return 1;
-            return 0; //default return value (no sorting)
-        } else {
-            var __a = parseFloat(_a);
-            var __b = parseFloat(_b);
-            return (__a - __b);
-        }
-    };
-
-/**
- * Hide or Show table Item
-*/
     $("section")
-        .off("click", "div.hide-show>div>span.item");
-    $("section").on("click", "div.hide-show>div>span.item",
-                    function(){
-                        show_hide_item($(this));
-                    });
+        .off("click", "table>thead>tr.items>th")
+        .on("click", "table>thead>tr.items>th", function(){
+            // get tbody
+            var $rows = $(this).parents("table").children("tbody").find("tr");
+            var col = $(this).index();
 
-    function show_hide_item($this){
-        var now_item = $this.data("item");
-        var selector = "table th." + now_item +
-                ", table td." + now_item;
-        if ($this.hasClass("show")){
-            $this.parents("div.Widget").find(selector).hide();
-            $this.removeClass("show");
-            $.cookie($this.parents("div.Widget").find("table").attr("path") + "-" +
-                     $this.data("group") + "-" + now_item + "-show", 0);
-        } else {
-            $this.parents("div.Widget").find(selector).show();
-            $this.addClass("show");
-            $.cookie($this.parents("div.Widget").find("table").attr("path") + "-" +
-                     $this.data("group") + "-" + now_item + "-show", 1);
-        }
-        correct_width_table($this.parents("div.Widget")
-                            .find("table.childrenTableWidget"));
-    }
+            // Sort
+            $rows.sort(function(a, b){
+                return compare(a, b, col) * sortOrder;
+            });
+            $(this).parents("table").children("tbody")
+                .empty().append($rows);
 
-    // show/hide group
-    $("section")
-        .off("click", "div.hide-show>div>span.group");
-    $("section").on("click", "div.hide-show>div>span.group",
-                    function(){
-                        var now_group = $(this).data("group");
-                        var selector = "table th[data-group=" + now_group + "]" +
-                                ", table td[data-group=" + now_group + "]";
-                        if ($(this).hasClass("show")){
-                            $(this).siblings().removeClass("show");
-                            $(this).siblings().each(function(){
-                                $.cookie($(this).parents("div.Widget").find("table").attr("path") + "-" +
-                                         $(this).data("group") + "-" +
-                                         $(this).data("item") + "-show", 0);
-                            });
-                            $(this).removeClass("show");
-                            $(this).parents("div.Widget").find(selector).hide();
-                        } else {
-                            $(this).siblings().addClass("show");
-                            $(this).siblings().filter(".item").each(function(){
-                                $.cookie($(this).parents("div.Widget").find("table").attr("path") + "-" +
-                                         $(this).data("group") + "-" +
-                                         $(this).data("item") + "-show", 1);
-                            });
-                            $(this).addClass("show");
-                            $(this).parents("div.Widget").find(selector).show();
-                        }
-                        correct_width_table($(this).parents("div.Widget").find("table.childrenTableWidget"));
-                    });
+            // update mark of sort order
+            var arrow = sortOrder === 1 ? "▲" : "▼";
+            $(this).parent().find('span').remove();
+            var span = $('<span>');
+            $(this).prepend(span);
+            $(this).find('span').text(arrow);
 
+            // change sort order
+            sortOrder *= - 1;
+        });
+}
 
+function enable_show_hide($wrap_table){
+    /** Hide or Show table Item
+     *
+     * Parameters
+     * ----------
+     * wrap_table : wrap set including tables
+     */
+    $wrap_table
+        .off("click", "div.hide-show>div>span.item")
+        .on("click", "div.hide-show>div>span.item", function(){
+            var now_item = $(this).data("item");
+            var selector = "table th." + now_item +
+                    ", table td." + now_item;
+            if ($(this).hasClass("show")){
+                $(this).parents("div.Widget").find(selector).hide();
+                $(this).removeClass("show");
+                $.cookie($(this).parents("div.Widget").find("table").attr("path") + "-" +
+                         $(this).data("group") + "-" + now_item + "-show", 0);
+            } else {
+                $(this).parents("div.Widget").find(selector).show();
+                $(this).addClass("show");
+                $.cookie($(this).parents("div.Widget").find("table").attr("path") + "-" +
+                         $(this).data("group") + "-" + now_item + "-show", 1);
             }
+            correct_width_table($(this).parents("div.Widget")
+                                .find("table.childrenTableWidget"));
+        })
+        .off("click", "div.hide-show>div>span.group")
+        .on("click", "div.hide-show>div>span.group", function(){
+            var now_group = $(this).data("group");
+            var selector = "table th[data-group=" + now_group + "]" +
+                    ", table td[data-group=" + now_group + "]";
+            if ($(this).hasClass("show")){
+                $(this).siblings().removeClass("show");
+                $(this).siblings().each(function(){
+                    $.cookie($(this).parents("div.Widget").find("table").attr("path") + "-" +
+                             $(this).data("group") + "-" +
+                             $(this).data("item") + "-show", 0);
+                });
+                $(this).removeClass("show");
+                $(this).parents("div.Widget").find(selector).hide();
+            } else {
+                $(this).siblings().addClass("show");
+                $(this).siblings().filter(".item").each(function(){
+                    $.cookie($(this).parents("div.Widget").find("table").attr("path") + "-" +
+                             $(this).data("group") + "-" +
+                             $(this).data("item") + "-show", 1);
+                });
+                $(this).addClass("show");
+                $(this).parents("div.Widget").find(selector).show();
+            }
+            correct_width_table($(this).parents("div.Widget").find("table.childrenTableWidget"));
+        });
+}
 
-function correct_width_table($table_object){
-    var $groups = $table_object.find("thead>tr.group>th");
-    // create group name list
-    var group_list = [];
-    $groups.each(function(){
-        group_list.push($(this).data("group"));
-    });
-    // count group number
-    var group_num = {};
-    for (var i in group_list){
-        group_num[group_list[i]] = 0;
-    }
-    // count each group number
-    $table_object.parent().find("div.hide-show div>span.item").each(function(){
-        if ($(this).hasClass("show")) {
-            for (var j in group_list){
-                if ($(this).data("group") == group_list[j]){
-                    group_num[group_list[j]] += 1;
-                    break;
-                }}}
-    });
-    // correct width of table.
-    $groups.each(function(){
-        $(this).attr("colspan", group_num[$(this).data("group")]);
-        if (group_num[$(this).data("group")] == 0){
-            $(this).hide();
-        } else {
-            $(this).show();
-        }});
+function ready_table(){
+    var $wrap_table_all = $("section#Widgets,nav#Projects");
+    var $wrap_table_widgets = $("section#Widgets");
+    add_menu($wrap_table_widgets);
+    enable_name_link($wrap_table_all);
+    enable_editable_comment($wrap_table_all);
+    enable_sort($wrap_table_widgets);
+    enable_show_hide($wrap_table_widgets);
 }
