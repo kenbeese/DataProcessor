07f4aa0a1c2e6fe1d2c1b0c21c0504f029f662c4
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2014-10-02 12:09:04 +0900

    Update test utility.

--- test/test_dataprocessor/utility.py
+++ test/test_dataprocessor/utility.py
@@ -21,64 +21,55 @@ class TestNodeListAndDir(unittest.TestCase):
     create node_list including following nodes.
 
 
-    ============    =========  ========
-    path            node_type  name
-    ============    =========  ========
-    tmpdir          project    tmpdir
-    tmpdir/run01    run        run01
-    tmpdir/run02    run        run02
-    tmpdir/run03    run        run03
-    ============    =========  ========
+    ===============   =========  ========
+    path              node_type  name
+    ===============   =========  ========
+    tmpdir/p1         project    p1
+    tmpdir/p1/run00   run        run00
+    tmpdir/p1/run01   run        run01
+    tmpdir/p1/run02   run        run02
+    tmpdir/p2         project    p2
+    tmpdir/p2/run00   run        run00
+    tmpdir/p2/run01   run        run01
+    ===============   =========  ========
 
     Attributes
     ----------
-    tempdir_paths : list
-        list of project root dir path
+    tempdir_path : str
+        temporally directory path
+    project_paths : list
+        list of project path
     node_list : list
-    rundir_nums : list
-        number list of rundir in `tempdir_paths`
 
     """
 
     def setUp(self):
-        self.tempdir_paths = []
-        self.rundir_nums = []
+        self.tempdir_path = tempfile.mkdtemp()
+        self.project_paths = []
         self.node_list = []
-        self._create_dir_and_node_list(rundir_num=3)
+        self._create_dir_and_run("p1", rundir_num=3)
+        self._create_dir_and_run("p2", rundir_num=2)
 
     def tearDown(self):
-        for tempdir_path in self.tempdir_paths:
-            shutil.rmtree(tempdir_path)
+        shutil.rmtree(self.tempdir_path)
 
-    def _create_dir_and_node_list(self, rundir_num=2):
-        """Create test directory and node_list.
+    def _create_project_and_run(self, project_name, rundir_num=2):
+        p_path = os.path.join(self.tempdir_path, project_name)
+        self._create_project_or_run(p_path)
 
-        Create one project directory and rundir_num rundirs.
-        Add self.tempdir_paths to project dir path.
+        for i in range(rundir_num):
+            path = os.path.join(p_path, "run%02d" % i)
+            self._create_project_or_run(path, type="run", parents=[p_path])
 
-        parameters
-        ----------
-        rundir_num : int, optional
-            number of rundir
+    def _create_project_or_run(self, path, type="project", parents=[],
+                               children=[]):
+        os.mkdir(path)
+        if type == "project":
+            self.project_paths.append(path)
 
-        """
-
-        self.rundir_nums.append(rundir_num)
-        self.tempdir_paths.append(tempfile.mkdtemp())
-        for tempdir_path in self.tempdir_paths:
-            node = {"path": tempdir_path,
-                    "type": "project",
-                    "name": os.path.basename(tempdir_path),
-                    "children": [],
-                    "parents": [], }
-            nodes.add(self.node_list, node)
-
-            for i in range(rundir_num):
-                path = os.path.join(tempdir_path, "run%02d" % i)
-                os.mkdir(path)
-                node = {"path": path,
-                        "type": "run",
-                        "name": os.path.basename(path),
-                        "children": [],
-                        "parents": [tempdir_path]}
-                nodes.add(self.node_list, node)
+        node = {"path": path,
+                "type": type,
+                "name": os.path.basename(path),
+                "children": children,
+                "parents": parents}
+        nodes.add(self.node_list, node)
