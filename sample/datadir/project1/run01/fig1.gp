642e724e06a9d8d166c1d39ac31628dd64c3f239
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2014-10-07 19:22:59 +0900

    Prepare for gh-pages.

--- sample/datadir/project1/run01/fig1.gp
+++
@@ -1,5 +0,0 @@
-
-set term png
-set output "fig1.png"
-
-p x*x
--- test/test_dataprocessor/test_pipes/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-import glob
-import os.path
-
-__all__ = [os.path.basename(f)[:-3]
-           for f in glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
-           if os.path.basename(f) != "__init__.py"]
-
-mod_list = [__import__(mod, globals(), locals(), [], - 1) for mod in __all__]
