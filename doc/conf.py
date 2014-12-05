df94ac693ea2d19a2eed1d9878c1ac776062010b
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2014-12-05 23:27:23 +0900

    Update sphinx configure.

--- doc/conf.py
+++ doc/conf.py
@@ -17,8 +17,8 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../test/test_dataprocessor'))
-sys.path.insert(0, os.path.abspath('../lib/dataprocessor'))
+sys.path.insert(0, os.path.abspath('../test'))
+sys.path.insert(0, os.path.abspath('../lib'))
 
 
 # -- General configuration -----------------------------------------------
