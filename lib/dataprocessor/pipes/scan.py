32c555dcbf1923fee2183b8dda6c4e086865b37f
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2014-12-14 13:36:37 +0900

    Enable to specify whitelist by string.

--- lib/dataprocessor/pipes/scan.py
+++ lib/dataprocessor/pipes/scan.py
@@ -17,7 +17,7 @@ def directory(node_list, root, whitelist, followlinks=False):
     ----------
     root : str
         Scan directories recursively under the directory `root`.
-    whitelist : list of str
+    whitelist : list of str or str
         Run node has one or more file or directory
         which satisfies run_node_dir/`whitelist`.
         And project nodes satisfy project_dir/run_node_dir/`whitelist`.
@@ -45,9 +45,10 @@ def directory(node_list, root, whitelist, followlinks=False):
     >>> node_list = directory([], "scandir_path", ["*.conf"])
 
     """
-
     root = path_expand(root)
     followlinks = boolenize(followlinks)
+    if isinstance(whitelist, str):
+        whitelist = [whitelist]
     scan_nodelist = []
     for path, dirs, files in os.walk(root, followlinks=followlinks):
         dirs.sort()
