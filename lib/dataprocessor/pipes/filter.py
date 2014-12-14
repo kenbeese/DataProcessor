89892c4fa0de81800beb07bea5a32589af6b09bc
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2014-12-15 06:20:16 +0900

    Change desc.

--- lib/dataprocessor/pipes/filter.py
+++ lib/dataprocessor/pipes/filter.py
@@ -8,16 +8,16 @@ def register(pipes_dics):
         "func": flt.project,
         "args": ["path"],
         "desc": """filter by project path.
-        This pipe filters out database.
-        In `dpmanip`, std output option '-o' should be used.
+        This pipe removes runs which don't belong to the project
+        and should be used with '-o' in dpmanip.
         """,
     }
     pipes_dics["filter_node_type"] = {
         "func": flt.node_type,
         "args": ["node_type"],
         "desc": """filter by node type.
-        This pipe filters out database.
-        In `dpmanip`, std output option '-o' should be used.
+        This pipe removes some nodes of the other type from the database.
+        and should be used with '-o' option in dpmanip.
         """,
     }
     pipes_dics["filter_prefix"] = {
@@ -26,7 +26,7 @@ def register(pipes_dics):
                   {"help": """Absolute path or relative path.
                    The relative path will be expanded to absolute path."""})],
         "desc": """filter by prefix_path.
-        This pipe filters out database.
-        In `dpmanip`, std output option '-o' should be used.
+        This pipe removes some runs, projects or figures from the database
+        and should be used with '-o' option in dpmanip.
         """
     }
