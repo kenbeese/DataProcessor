839228dce1ddf7cf129c5fd23369b254632c0857
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2014-12-15 03:22:17 +0900

    Add filter by path pipe.

--- lib/dataprocessor/pipes/filter.py
+++ lib/dataprocessor/pipes/filter.py
@@ -7,10 +7,21 @@ def register(pipes_dics):
     pipes_dics["filter_project"] = {
         "func": flt.project,
         "args": ["path"],
-        "desc": "filter by project path",
+        "desc": """filter by project path.
+        This pipe reduce database. In `dpmanip`, std output option '-o' should be used.
+        """,
     }
     pipes_dics["filter_node_type"] = {
         "func": flt.node_type,
         "args": ["node_type"],
-        "desc": "filter by node type",
+        "desc": """filter by node type.
+        This pipe reduce database. In `dpmanip`, std output option '-o' should be used.
+        """,
+    }
+    pipes_dics["filter_path"] = {
+        "func": flt.prefix_path,
+        "args": ["pre_path"],
+        "desc": """filter by prefix_path.
+        This pipe reduce database. In `dpmanip`, std output option '-o' should be used.
+        """
     }
