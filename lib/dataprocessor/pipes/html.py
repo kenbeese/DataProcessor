b8022ecb6507f89fe0c46db3352cab38ff878cb8
Toshiki Teramura <toshiki.teramura@gmail.com>
2014-12-16 02:23:43 +0900

    Use dataframe to show parameter table

--- lib/dataprocessor/pipes/html.py
+++ lib/dataprocessor/pipes/html.py
@@ -1,27 +1,47 @@
 # -*- coding: utf-8 -*-
 
+import json
 import os.path as op
 from jinja2 import Template
 
 from ..utility import check_directory
 from ..filter import node_type
+from ..dataframe import get_project
 
 template_dir = check_directory(op.join(__file__, "../../../../template/"))
 
 
-def project_table(node_list):
+def projects(node_list):
     with open(op.join(template_dir, "project_table.html"), "r") as f:
         template = Template(f.read())
     projects = node_type(node_list, "project")
-    print(template.render(projects=projects))
+    res = {
+        "paths": [node["path"] for node in projects],
+        "html": template.render(projects=projects),
+    }
+    print(json.dumps(res))
+    return node_list
+
+
+def project_table(node_list, path):
+    df = get_project(node_list, path, properties=["comment"])
+    # TODO replace with original HTML template
+    print(df.to_html(classes=["table", "table-striped"]))
     return node_list
 
 
 def register(dicts):
-    dicts["project_table"] = {
-        "func": project_table,
+    dicts["projects_html"] = {
+        "func": projects,
         "args": [],
         "kwds": [],
         "desc": "output project HTML table",
+        "output": "json",
+    }
+    dicts["project_table_html"] = {
+        "func": project_table,
+        "args": ["path"],
+        "kwds": [],
+        "desc": "output HTML table",
         "output": "html",
     }
