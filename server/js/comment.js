82767c82a70600496e5a4a701cb23cf5eadc3357
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2014-12-20 04:09:54 +0900

    Instead of <input>, <textarea> is used.

--- server/js/comment.js
+++ server/js/comment.js
@@ -15,14 +15,14 @@ function enable_editable_comment() {
   f_click = function(){
     var comment = $(this).text();
     var path = $(this).attr("dp-path");
-    $(this).empty().off("click")
-    $("<input>")
+    $(this).empty().off("click");
+    $("<textarea>")
       .addClass("dp-comment")
-      .attr("value", comment)
+      .val(comment)
       .attr("dp-path", path)
       .appendTo(this)
       .focus();
-  }
+  };
   f_blur = function(){
     var comment = $(this).val();
     var path = $(this).attr("dp-path");
@@ -31,10 +31,10 @@ function enable_editable_comment() {
       .empty()
       .append(comment)
       .on("click", f_click);
-  }
+  };
   $("td.dp-comment")
     .off("click")
     .on("click", f_click)
-    .off("blur", "input.dp-comment")
-    .on("blur", "input.dp-comment", f_blur);
+    .off("blur", "textarea.dp-comment")
+    .on("blur", "textarea.dp-comment", f_blur);
 }
