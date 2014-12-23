e07c6ad44c80300ab994128e4c95962a860f3f05
Toshiki Teramura <toshiki.teramura@gmail.com>
2014-12-24 00:52:37 +0900

    Use transparent overlay

--- server/js/setting.js
+++ server/js/setting.js
@@ -3,5 +3,9 @@ var animation_speed = "fast"
 var api_url = "/cgi-bin/api.cgi"
 var block_setting = {
   message: false,
-  showOverlay: false,
+  overlayCSS: {
+      backgroundColor: '#000',
+      opacity: 0.0,
+      cursor: 'wait'
+  },
 }
