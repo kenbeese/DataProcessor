e615ef23dc5fbe859bd87de5da9c1e8348ce49ad
Toshiki Teramura <toshiki.teramura@gmail.com>
2014-12-27 22:09:48 +0900

    Use pipe.type decorator for configure pipes

--- lib/dataprocessor/pipes/configure.py
+++ lib/dataprocessor/pipes/configure.py
@@ -1,28 +1,24 @@
 # coding=utf-8
-"""Pipes of configure."""
-import os.path
+
+import os.path as op
 from ConfigParser import SafeConfigParser
 
-from ..utility import read_configure
+from .. import pipe
+from ..utility import read_configure, check_file
+from ..exception import DataProcessorError as dpError
 
 
-def add(node_list, filename, section="parameters"):
-    """
-    Add configure to node_list.
+@pipe.type("run")
+def add(node, filename, section="parameters"):
+    """ Load configure
 
     Parameters
     ----------
     filename : str
         filename of parameter configure file
         If file is not exists, add null list.
-
     section : str
         Specify section name in configure file.
-
-    Returns
-    -------
-    list
-        node_list which is a list of dict.
 
     Examples
     --------
@@ -31,32 +27,20 @@ def add(node_list, filename, section="parameters"):
     >>> add(node_list, "configure.conf", "defaults") # doctest:+SKIP
 
     """
-    node_key = "configure"
-    for node in node_list:
-        confpath = os.path.join(node["path"], filename)
-        conf_d = {}
-        if os.path.exists(confpath):
-            conf = SafeConfigParser()
-            conf.optionxform = str
-            conf.read(confpath)
-            for key, var in conf.items(section):
-                conf_d[key] = var
-        else:
-            Warning("parameter file is not exists.")
-        if not node_key in node:
-            node[node_key] = conf_d
-        else:
-            for key in conf_d:
-                if key in node[node_key]:
-                    Warning("overwrite configures")
-            node[node_key].update(conf_d)
+    confpath = check_file(op.join(node["path"], filename))
 
-    return node_list
+    conf = SafeConfigParser()
+    conf.optionxform = str
+    conf.read(confpath)
+
+    if "configure" not in node:
+        node["configure"] = {}
+    node["configure"].update(dict(conf.items(section)))
 
 
-def no_section(node_list, filename, split_char="=", comment_char=["#"]):
-    """
-    Add configure to node_list.
+@pipe.type("run")
+def no_section(node, filename, split_char="=", comment_char=["#"]):
+    """ Load configure
 
     Parameters
     ----------
@@ -68,11 +52,6 @@ def no_section(node_list, filename, split_char="=", comment_char=["#"]):
     comment_char : str
         Specify the comment line signal char.
 
-    Returns
-    -------
-    list
-        node_list which is a list of dict.
-
     Examples
     --------
     >>> no_section(node_list, "foo.conf") # doctest:+SKIP
@@ -81,16 +60,12 @@ def no_section(node_list, filename, split_char="=", comment_char=["#"]):
     ... # doctest:+SKIP
 
     """
-    for node in node_list:
-        path = node["path"]
-        cfg_path = os.path.join(path, filename)
-        if not os.path.exists(cfg_path):
-            continue
-        cfg = read_configure(cfg_path, split_char, comment_char)
-        if "configure" not in node:
-            node["configure"] = {}
-        node["configure"].update(cfg)
-    return node_list
+    path = node["path"]
+    cfg_path = check_file(op.join(path, filename))
+    cfg = read_configure(cfg_path, split_char, comment_char)
+    if "configure" not in node:
+        node["configure"] = {}
+    node["configure"].update(cfg)
 
 
 def register(pipes_dics):
