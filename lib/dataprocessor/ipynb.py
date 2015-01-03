32ec106985c2a596a2c8e889c2da9acd61f97428
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-01-03 18:05:50 +0900

    Gather ipynb files

--- lib/dataprocessor/ipynb.py
+++ lib/dataprocessor/ipynb.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os.path as op
 import psutil
+import json
 from .utility import check_file
 from .exception import DataProcessorError as dpError
 
@@ -65,3 +67,15 @@ def resolve_url(ipynb_path):
         return "http://localhost:{port}/notebooks/{postfix}".format(**note)
     raise dpError("No valid Notebook found. "
                   "Please stand notebook server on the parent directory.")
+
+
+def resolve_name(ipynb_path):
+    ipynb_path = check_file(ipynb_path)
+    try:
+        with open(ipynb_path, "r") as f:
+            name = json.load(f)["metdata"]["name"]
+    except KeyError:
+        name = ""
+    if not name:
+        name = op.basename(ipynb_path)
+    return name
--- lib/dataprocessor/pipes/ipynb.py
+++ lib/dataprocessor/pipes/ipynb.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
 
+import os.path as op
 import webbrowser
-from ..ipynb import resolve_url
+from glob import glob
+from .. import nodes
+from ..utility import path_expand
+from ..ipynb import resolve_url, resolve_name
+from ..exception import DataProcessorError as dpError
 
 
 def start(nl, ipynb_path):
@@ -32,9 +37,33 @@ def start(nl, ipynb_path):
     return nl
 
 
+def gather(node_list, pattern="*.ipynb"):
+    """
+    Search ipynb file by its extention
+    """
+    for node in node_list:
+        path = node["path"]
+        for fn in glob(op.join(path, pattern)):
+            fn = path_expand(fn)
+            node = {
+                "path": fn,
+                "type": "ipynb",
+                "name": resolve_name(fn),
+                "parents": [path, ],
+                "children": [],
+            }
+            nodes.add(node_list, node)
+    return node_list
+
+
 def register(pipes_dics):
     pipes_dics["start_ipynb"] = {
         "func": start,
         "args": ["ipynb_path"],
         "desc": "start .ipynb in standing notebook",
+    }
+    pipes_dics["gather_ipynb"] = {
+        "func": gather,
+        "args": [],
+        "desc": "gather ipynb files",
     }
