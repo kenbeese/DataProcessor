6dece465148706f084593c02faf9693c7e9a88bf
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-01-23 01:43:02 +0900

    Check does not filter out

--- lib/dataprocessor/tests/test_pipe.py
+++ lib/dataprocessor/tests/test_pipe.py
@@ -8,7 +8,6 @@ from ..exception import DataProcessorError as dpError
 
 @pipe.wrap
 def wrap_noreturn(node):
-    """Not arrowed"""
     pass
 
 
@@ -22,6 +21,11 @@ def wrap_raise(node):
     raise RuntimeError("cannot catch")
 
 
+@pipe.file
+def file_pipe(node):
+    return node
+
+
 class TestPipe(TestNodeListAndDir):
 
     def test_noreturn(self):
@@ -33,3 +37,10 @@ class TestPipe(TestNodeListAndDir):
         self.assertEquals(nl, self.node_list)
         with self.assertRaises(RuntimeError):
             wrap_raise(self.node_list)
+
+    def test_nofilter(self):
+        """
+        Do not filter node_list.
+        """
+        nl = file_pipe(self.node_list)
+        self.assertEquals(nl, self.node_list)
