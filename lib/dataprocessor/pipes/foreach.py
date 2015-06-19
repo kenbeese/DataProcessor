dea601eb2fc1d3c130082dac928dc52e9fc7b867
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-06-19 20:32:52 +0900

    Add project specifier

--- lib/dataprocessor/pipes/foreach.py
+++ lib/dataprocessor/pipes/foreach.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from .. import runner, pipe
+from .. import runner, pipe, basket
+from .. import filter as dpfilter
 
 
 @pipe.directory
@@ -10,9 +11,15 @@ def foreach_dir(node, args):
 
 
 @pipe.type("run")
-def foreach_run(node, args):
+def _foreach_run(node, args):
     runner.sync(args, node["path"])
     return node
+
+
+def foreach_run(node_list, args, project=None):
+    if project:
+        node_list = dpfilter.project(node_list, basket.resolve_project_path(project))
+    return _foreach_run(node_list, args)
 
 
 @pipe.type("project")
@@ -26,6 +33,9 @@ def register(pipes_dics):
         "nargs": "+",
         "help": "command (use '--' when you use option, e.g. foreach -- ls -lt)",
     })
+    project = ("project", {
+        "help": "project name or path",
+    })
     pipes_dics["foreach"] = {
         "func": foreach_dir,
         "args": [args],
@@ -34,6 +44,7 @@ def register(pipes_dics):
     pipes_dics["foreach_run"] = {
         "func": foreach_run,
         "args": [args],
+        "kwds": [project],
         "desc": "Execute a command in run directories",
     }
     pipes_dics["foreach_project"] = {
